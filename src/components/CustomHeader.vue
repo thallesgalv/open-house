6c1aa2ea32c5e78875c081f5ff824f350e9448bd
Thalles Galvão <thallesgalv@gmail.com>
2022-10-08 19:10:17 -0300

    update dist

--- src/components/CustomHeader.vue
+++
@@ -1,9 +0,0 @@
-<script setup></script>
-
-<template>
-  <header class="p-2 mb-6 bg-white shadow-lg">
-    <p class="w-full text-2xl font-semibold text-center text-primary">
-      Open House Lu&Thalles 🏠
-    </p>
-  </header>
-</template>
--- src/composables/useCurrency.js
+++
@@ -1,9 +0,0 @@
-export const useCurrency = (value) => {
-  if (typeof value === 'string' || typeof value === 'number') {
-    const valueToFormat = +value
-    return valueToFormat.toLocaleString('pt-BR', {
-      style: 'currency',
-      currency: 'BRL'
-    })
-  }
-}
